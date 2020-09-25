fd69d3f0da3e01a58ca93f2c64403649d0d54d34
nbarlowATI <nbarlow@turing.ac.uk>
2020-09-25 16:14:42 +0100

    for testing, just look at first 10 rows

--- scholar_citations.py
+++ scholar_citations.py
@@ -96,7 +96,8 @@ def main():
 
     input_df = make_dataframe_from_input(args.input_csv)
     results = []
-    for i in range(len(input_df)):
+#    for i in range(len(input_df)):
+    for i in range(10):
         row = input_df.loc[i]
         result = process_row(i, row)
         results.append(result)
